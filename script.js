9dbaa2753969112442e3335bc5735ac97b5a7f7e
aangupta <aanchalgupta457@gmail.com>
2021-02-05 00:50:34 +0530

    changed the layout

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const ucase = document.querySelector("#touppercase");
 const charcount = document.querySelector("#charactercount");
 const reverse = document.querySelector("#reverse");
 const wordcount = document.querySelector("#wordcount");
-const input = document.querySelector("#inputstring");
+const input = document.querySelector("#input-text");
 
 lcase.addEventListener('click',()=>{
-    document.querySelector("#outputstring").value = input.value.toLowerCase();
+    document.querySelector("#output-text").value = input.value.toLowerCase();
 });
 ucase.addEventListener('click',()=>{
-    document.querySelector("#outputstring").value = input.value.toUpperCase();
+    document.querySelector("#output-text").value = input.value.toUpperCase();
 });
 charcount.addEventListener('click',()=> {
-    document.querySelector("#outputstring").value = parseInt(input.value.length);
+    document.querySelector("#output-text").value = parseInt(input.value.length);
 });
 reverse.addEventListener('click',()=> {
-    document.querySelector("#outputstring").value = input.value.split("").reverse().join("");
+    document.querySelector("#output-text").value = input.value.split("").reverse().join("");
 });
 wordcount.addEventListener('click',()=> {
-    document.querySelector("#outputstring").value = parseInt(input.value.split(" ").length);
+    document.querySelector("#output-text").value = parseInt(input.value.split(" ").length);
 });
